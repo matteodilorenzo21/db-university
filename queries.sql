a4205b490431098d2841e3f718cb17a949d99f63
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-29 17:44:36 +0200

    Task #7

--- queries.sql
+++ queries.sql
@@ -27,7 +27,7 @@ SELECT * FROM `degrees` WHERE `level` LIKE 'm%';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-
+SELECT * FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
