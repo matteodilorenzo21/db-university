aa69d56c34ae18d5c8bb78464a818be250640035
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-29 17:33:24 +0200

    Task #4

--- queries.sql
+++ queries.sql
@@ -15,7 +15,7 @@ SELECT * FROM `students` WHERE YEAR(DATEDIFF(NOW(), `date_of_birth`)) > 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-
+SELECT * FROM `courses` WHERE `period` LIKE 'I s%' AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
