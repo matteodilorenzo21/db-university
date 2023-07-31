acec4b5653f9ff3c071d38b461657d2451c28c94
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-31 17:49:41 +0200

    Join #1

--- queries.sql
+++ queries.sql
@@ -33,6 +33,7 @@ SELECT * FROM `departments`;
 
 SELECT COUNT(*) FROM `teachers` WHERE `phone` IS NULL;
 
+
 -- QUERY CON GROUP BY --
 
 
@@ -51,3 +52,35 @@ SELECT `exam_id`, ROUND(AVG(`vote`)) FROM `exam_student` GROUP BY `exam_id`;
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
 SELECT `department_id`, COUNT(*) FROM `degrees` GROUP BY `department_id`;
+
+
+-- QUERY CON JOIN --
+
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT S.* FROM `degrees` AS D JOIN `students` AS S ON D.`id` = S.`degree_id` WHERE D.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+
